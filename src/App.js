318f1dbd89bda8b215d09a1a023276a1889d5285
JaakLipp <lippert.22j@gmail.com>
2023-05-24 11:00:07 -0400

    idk

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <NavBar />
         <div id="page-body">
           <Routes>
-            <Route path="/test01" element={<HomePage />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/" element={<HomePage />} />
             <Route path="*" element={<NotFoundPage />} />
 
